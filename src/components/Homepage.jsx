b18e1b5bcc5169600260e184db27a79d373ca8a9
Piyus Sagar <piyushsagarofficial@gmail.com>
2022-11-05 22:00:25 +0530

    link added

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -12,8 +12,10 @@ function Homepage() {
                 <img src="https://res.cloudinary.com/dkr75ykb4/image/upload/v1667657020/medhub/hero_ichj4k.png" alt="heroimage" />
                 <div className="text-container">
                     <h2>MedHub Welcomes You</h2>
-                    <p>A Place to Assist your Medical Storage and help  you to take care the needs and requirement of your customers</p>
-                    <button>Get Start</button>
+                    <p>A place to assist your Medical storage and help  you to take care the needs and requirement for your customers</p>
+                    <div className="btn">
+                        <a className="btn" href="https://piyush362.github.io/">Click here</a>
+                    </div>
                 </div>
             </div>
         </div>
